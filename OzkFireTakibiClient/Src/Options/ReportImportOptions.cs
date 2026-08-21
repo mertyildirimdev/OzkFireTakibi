f6b2cf3a3c434114c93320c53d87b286b2390082
Mert YILDIRIM <business.mertyildirim@gmail.com>
2026-08-21 16:20:04 +0300

    Update RaporDetay component to support comparison subheaders and enhance field comparability; increase max file size limit in ReportImportOptions

--- OzkFireTakibiClient/Src/Options/ReportImportOptions.cs
+++ OzkFireTakibiClient/Src/Options/ReportImportOptions.cs
@@ -4,6 +4,6 @@ public sealed class ReportImportOptions
 {
     public const string SectionName = "ReportImport";
 
-    public long MaxFileSizeBytes { get; set; } = 10 * 1024 * 1024;
+    public long MaxFileSizeBytes { get; set; } = 20 * 1024 * 1024;
     public int HistoryPageSize { get; set; } = 50;
 }
